c7525e60305483aa1f23b829d9050eb68bb86313
AshokNarvaneni <anarvaneni@evoketechnologies.com>
2024-02-15 16:50:49 -0500

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Ashok Narvaneni on 15/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
